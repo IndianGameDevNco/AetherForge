7965fab63b7291dc502a4422364e9dd3f4a31b8b
IndianGameDevNco <bhasins2020@gmail.com>
2025-02-25 13:32:48 +0000

    Got rid of of problematic middleware made by Copilot

--- core/aetherforge/settings.py
+++ core/aetherforge/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'aetherforge.middleware.AdminAccessMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
